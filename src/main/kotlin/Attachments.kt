5898318c37f4451750afdb7fa9a62d4ab2cb1363
Andrei Razborov <andreyrd94@gmail.com>
2023-01-03 22:25:22 +0300

    Interface Attachment

---
+++ src/main/kotlin/Attachments.kt
@@ -0,0 +1,92 @@
+interface Attachment {
+    val type: String
+}
+class Note(
+    var id:Int,
+    var ownerId: Int,
+    var title: String,
+    var text: String,
+    var date: Int
+){
+    override fun toString(): String {
+        return "Note(id=$id, ownerId=$ownerId, title='$title', text='$text', date=$date)"
+    }
+}
+class NoteAttachment(override val type: String = "note", val note: Note) : Attachment {
+    override fun toString(): String {
+        return "NoteAttachment(type='$type', note=$note)"
+    }
+}
+class Doc(
+    var id:Int,
+    var ownerId: Int,
+    var title: String,
+    var size: Int,
+    var ext: String
+) {
+    override fun toString(): String {
+        return "Doc(id=$id, ownerId=$ownerId, title='$title', size=$size, ext='$ext')"
+    }
+}
+class DocAttachment(override val type: String = "doc", val doc: Doc) : Attachment {
+    override fun toString(): String {
+        return "DocAttachment(type='$type', doc=$doc)"
+    }
+}
+class Video(
+    var id:Int,
+    var ownerId: Int,
+    var title: String,
+    var description: String,
+    var duration: Int
+){
+    override fun toString(): String {
+        return "Video(id=$id, ownerId=$ownerId, title='$title', description='$description', duration='$duration')"
+    }
+}
+class VideoAttachment(override val type: String = "video", val video: Video) : Attachment {
+    override fun toString(): String {
+        return "VideoAttachment(type='$type', video=$video)"
+    }
+}
+class PostedPhoto(
+    var id: Int,
+    var ownerId: Int,
+    var photo130: String,
+    var photo604: String
+){
+    override fun toString(): String {
+        return "PostedPhoto(id=$id, ownerId=$ownerId, photo130='$photo130', photo604='$photo604')"
+    }
+}
+
+class PostedPhotoAttachment(override val type: String = "posted_photo", val postedPhoto: PostedPhoto) : Attachment {
+    override fun toString(): String {
+        return "PostedPhotoAttachment(type='$type', postedPhoto=$postedPhoto)"
+    }
+}
+
+class Audio(
+    var id: Int,
+    var ownerId: Int,
+    var artist: String,
+    var title: String,
+    var duration: Int,
+    var url: String,
+    var lyricsId: Int,
+    var albumId: Int,
+    var genreId: Int,
+    var date: Int,
+    var noSearch: Boolean,
+    var isHq: Boolean
+) {
+    override fun toString(): String {
+        return "Audio(id=$id, ownerId=$ownerId, artist='$artist', title='$title', duration=$duration, url='$url', lyricsId=$lyricsId, albumId=$albumId, genreId=$genreId, date=$date, noSearch=$noSearch, isHq=$isHq)"
+    }
+}
+
+class AudioAttachment(override val type: String = "audio", val audio: Audio) : Attachment {
+    override fun toString(): String {
+        return "AudioAttachment(type='$type', audio=$audio)"
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,66 @@
 
-
 data class Post(
-    val id: Int,
-    val ownerId: Int,
-    val fromId: Int,
-    val createdBy: Int,
-    val date: Int,
-    val text: String,
-    val postType: String,
-    val comments: Comments,
-    val likes: Likes,
-    val isFavorite: Boolean
-)
+     var id: Int,
+     var ownerId: Int,
+     var fromId: Int,
+     var createdBy: Int,
+     var date: Int,
+     var text: String,
+     var postType: String,
+     var comments: Comments?,
+     var likes: Likes?,
+     var isFavorite: Boolean,
+     var attachments: Array<Attachment> = emptyArray()
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Post
+
+        if (id != other.id) return false
+        if (ownerId != other.ownerId) return false
+        if (fromId != other.fromId) return false
+        if (createdBy != other.createdBy) return false
+        if (date != other.date) return false
+        if (text != other.text) return false
+        if (postType != other.postType) return false
+        if (comments != other.comments) return false
+        if (likes != other.likes) return false
+        if (isFavorite != other.isFavorite) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + ownerId
+        result = 31 * result + fromId
+        result = 31 * result + createdBy
+        result = 31 * result + date
+        result = 31 * result + text.hashCode()
+        result = 31 * result + postType.hashCode()
+        result = 31 * result + (comments?.hashCode() ?: 0)
+        result = 31 * result + (likes?.hashCode() ?: 0)
+        result = 31 * result + isFavorite.hashCode()
+        return result
+    }
+}
 
 object WallService{
     private var posts = emptyArray<Post>()
     private var nextId = 1
     fun add(post: Post): Post{
-        posts += post.copy(id = nextId++)
+        post.id = nextId++
+        posts += post
         return posts.last()
     }
 
     fun update(post: Post): Boolean {
         for ((index,checkPost) in posts.withIndex()){
             if (post.id == checkPost.id){
-                posts[index] = post.copy(id = checkPost.id, date = checkPost.date)
+                post.date = checkPost.date
+                posts[index] = post
                 return true
             }
         }
@@ -41,20 +77,13 @@ object WallService{
     }
 
 }
-class Comments {
-    private var count: Int = 0
-    private var canPost: Boolean = false
-    private var groupsCanPost: Boolean = false
-    private var canClose: Boolean = false
-    private var canOpen: Boolean = false
-
-    constructor(count: Int, canPost: Boolean, groupsCanPost: Boolean, canClose: Boolean, canOpen: Boolean) {
-        this.count = count
-        this.canPost = canPost
-        this.groupsCanPost = groupsCanPost
-        this.canClose = canClose
-        this.canOpen = canOpen
-    }
+class Comments(
+    private var count: Int,
+    private var canPost: Boolean,
+    private var groupsCanPost: Boolean,
+    private var canClose: Boolean,
+    private var canOpen: Boolean
+) {
 
     override fun toString(): String {
         return "Comments(count=$count, canPost=$canPost, groupsCanPost=$groupsCanPost, canClose=$canClose, canOpen=$canOpen)"
@@ -63,18 +92,7 @@ class Comments {
 
 }
 
-class Likes {
-    var count: Int = 0
-    var userLikes: Boolean = false
-    var canLike: Boolean = false
-    var canPublish: Boolean = false
-
-    constructor(count: Int, userLikes: Boolean, canLike: Boolean, canPublish: Boolean) {
-        this.count = count
-        this.userLikes = userLikes
-        this.canLike = canLike
-        this.canPublish = canPublish
-    }
+class Likes(var count: Int, var userLikes: Boolean, var canLike: Boolean, var canPublish: Boolean) {
 
     override fun toString(): String {
         return "Likes(count=$count, userLikes=$userLikes, canLike=$canLike, canPublish=$canPublish)"
@@ -82,23 +100,35 @@ class Likes {
 
 }
 
-fun main(args: Array<String>) {
+fun main() {
+    val attachment = arrayOf<Attachment>(AudioAttachment("audio",Audio(1,1,"ttt","ttt",1,"ttt",1,1,1,
+        1,true,true)),
+        PostedPhotoAttachment("posted_photo",PostedPhoto(1,1,"tt","tt"))
+    )
+    val attachment2 = arrayOf<Attachment>(VideoAttachment("video",Video(1,1,"V","vvv",1)),
+        DocAttachment("doc",Doc(1,1,"doc",120,"e"))
+        )
+
     val post = Post(0,1,1,1,1
         ,"Hellow","Hellow"
-        ,Comments(0,false,false,false,false)
-        ,Likes(0,false,false,false),true)
+        ,null
+        ,Likes(0,false,false,false),true, attachment
+    )
     val post2 = Post(0,2,2,2,1
         ,"Hellow 2 ","Hellow"
         ,Comments(1,false,false,false,false)
-        ,Likes(1,false,false,false),true)
+        ,null,true,attachment2)
     WallService.add(post)
     WallService.add(post2)
     WallService.printAll()
+    val attachment3 = arrayOf<Attachment>(NoteAttachment("note",Note(1,1,"tit","text",11)))
     val post3 = Post(1,2,2,2,5
         ,"Hel ","Hel"
         ,Comments(4,false,false,false,false)
-        ,Likes(4,false,false,false),true)
+        ,Likes(4,false,false,false),true, attachment3
+    )
     WallService.update(post3)
     println("----------------------")
     WallService.printAll()
+
 }
